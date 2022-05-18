6306d3b1845fa34a8c02548e6c074a922dffcae8
Katarína Badžanová <49408989+Katcoolka@users.noreply.github.com>
2022-05-18 20:07:03 +0200

    Initial commit

---
+++ src/main/java/com/example/musicui/Main.java
@@ -0,0 +1,46 @@
+package com.example.musicui;
+
+import com.example.musicui.model.Datasource;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Controller controller = fxmlLoader.getController();
+        controller.listArtists();
+
+        primaryStage.setTitle("Music Database");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        if(!Datasource.getInstance().open()) {
+            System.out.println("FATAL ERROR: Couldn't connect to database");
+            Platform.exit();
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        Datasource.getInstance().close();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
